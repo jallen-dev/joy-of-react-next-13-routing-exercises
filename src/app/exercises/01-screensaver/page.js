168670584ecac266d367f976d992abaf2740314a
Justin Allen <jallen@jallen.dev>
2023-10-19 00:57:07 -0400

    Complete exercise 1

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,12 +1,26 @@
 import React from "react";
 
-import ScreenSaver from "../../../components/ScreenSaver";
+import Link from "next/link";
 
 function ScreenSaverExercise() {
   return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
-    </main>
+    <div>
+      Choose your color:
+      <ul>
+        <li>
+          <Link href="/exercises/01-screensaver/coral">Coral</Link>
+        </li>
+        <li>
+          <Link href="/exercises/01-screensaver/darkorchid">Dark Orchid</Link>
+        </li>
+        <li>
+          <Link href="/exercises/01-screensaver/goldenrod">Goldenrod</Link>
+        </li>
+        <li>
+          <Link href="/exercises/01-screensaver/seagreen">Sea Green</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
 
